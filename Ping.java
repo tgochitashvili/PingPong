b7d9f89c2f9e3fa121876c2875e21fb9f44ebded
tgochitashvili <tazo.gochitashvili@gmail.com>
2022-09-03 01:52:05 +0400

    import cleanup

--- Ping.java
+++ Ping.java
@@ -2,7 +2,6 @@ import java.net.UnknownHostException;
 import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
