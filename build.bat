7d4f5532725c393cfe6bf15ecb9bcad9d22c2a37
tgochitashvili <tazo.gochitashvili@gmail.com>
2022-09-03 01:40:56 +0400

    Added sample windows build and run configs

---
+++ build.bat
@@ -0,0 +1,2 @@
+javac *.java
+"C:\Program Files\Java\jdk-18.0.2.1\bin\jar.exe" cfe  Pong.jar Ping *.class
---
+++ run.bat
@@ -0,0 +1,3 @@
+call build.bat
+
+java -jar Pong.jar threads=100 readTimeout=10000 connTimeout=10000 serverpath=servers.txt urlpath=urls.txt token=SERVER_TOKEN
