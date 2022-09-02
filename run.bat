e425166effc9751617fa1980e967a6b058b36257
tgochitashvili <tazo.gochitashvili@gmail.com>
2022-09-03 01:44:42 +0400

    Changed threadcount

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 call build.bat
 
-java -jar Pong.jar threads=100 readTimeout=10000 connTimeout=10000 serverpath=servers.txt urlpath=urls.txt token=SERVER_TOKEN
+java -jar Pong.jar threads=20 readTimeout=10000 connTimeout=10000 serverpath=servers.txt urlpath=urls.txt token=SERVER_TOKEN
