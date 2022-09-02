e9c82a7cfeba1c854d9f0b1b3cabe3aa1bc5f52c
tgochitashvili <tazo.gochitashvili@gmail.com>
2022-09-03 01:52:00 +0400

    Added support for servernames

--- ProcessNode.java
+++ ProcessNode.java
@@ -1,10 +1,20 @@
 import java.util.LinkedList;
 public class ProcessNode{
-    LinkedList<Process> processList;
+    public LinkedList<Process> processList;
+    public String serverName;
+    public ProcessNode(LinkedList<Process> processList, String serverName){
+        this.processList = new LinkedList<Process>(processList);
+        this.serverName = serverName;
+    }
     public ProcessNode(LinkedList<Process> processList){
         this.processList = new LinkedList<Process>(processList);
+        this.serverName = "";
+    }
+    public ProcessNode(String serverName){
+        this.serverName = serverName;
     }
     public ProcessNode(){
         this.processList = new LinkedList<Process>();
+        this.serverName = "";
     }
 }
