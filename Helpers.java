433caf6b07a482858b458b63edf727c1a04dd0a3
tgochitashvili <tazo.gochitashvili@gmail.com>
2022-10-22 12:33:08 +0400

    Fixed powershell clear

--- Helpers.java
+++ Helpers.java
@@ -9,7 +9,6 @@ import java.util.Date;
 import java.util.LinkedList;
 import java.util.HashMap;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.io.IOException;
@@ -30,40 +29,60 @@ public class Helpers {
             }
             argMap.put(splitArg[0],splitArg[1]);
         }
+        if(!argMap.containsKey("urlpath")){
+            argMap.putIfAbsent("valid", "false");
+        }
 
+        if(argMap.containsKey("serverpath")){
+            if(!argMap.containsKey("token")){
+                argMap.putIfAbsent("valid", "false");
+            }
+        }
         return argMap;
     }
 
-    public static void CLEARSCREEN(){
-        System.out.printf("\033[H\033[2J","");
+    public static void CLEARSCREEN() {
+        try {
+            if (System.getProperty("os.name").contains("Windows"))
+                new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+            else{
+                String[] stringArray = {"clear"};
+                Runtime.getRuntime().exec(stringArray);
+            }
+        } catch (IOException | InterruptedException ex) {}
+    }
+
+    public static void printInfoAndQuit(){
+        CLEARSCREEN();
+        System.out.println("Please enter some arguments:");
+        System.out.println("\t threads=x\t\t - Amount of threads to use for pinging (default: hardware threads * 5");
+        System.out.println("\t readtimeout=x\t\t - timeout limit for the response in milliseconds (default: 1000)");
+        System.out.println("\t conntimeout=x\t\t - timeout limit for the connection in milliseconds (default: 1000)");
+        System.out.println("\t serverpath=x\t\t - path to the file containing the server list, only works with a tokenized url list, requires token");
+        System.out.println("\t urlpath=x\t\t - mandatory, path to the file containing either full or tokenized URLs");
+        System.out.println("\t token=x\t\t - mandatory for serverpath, token to look for in the urls and replace with the real servers, default is empty and will ignore the server list");
+        
+        System.exit(-1);
     }
 
     public static void trackThreadPoolWrapperProgress(LinkedList<ThreadPoolWrapper> poolList, long timeout) throws InterruptedException{
         long start = System.currentTimeMillis();
         boolean allDone;
-        int stringLength;
         while((System.currentTimeMillis()-start) < timeout){
-            stringLength = 0;
             allDone = true;
             for(ThreadPoolWrapper threadPoolWrapper: poolList){
                 long numTasks = threadPoolWrapper.getExecutor().getTaskCount();
-                int progress = 0;
-                for(Future<?> future: threadPoolWrapper.getFutures()){
-                    progress+=future.isDone()?1:0;
-                }
+                long progress = threadPoolWrapper.getExecutor().getCompletedTaskCount();
                 boolean currentDone = (progress >= numTasks);
                 allDone &= currentDone;
                 String toPrint = "[" + threadPoolWrapper.getServerName() + "]\t\t Progress : " + progress + "/" + numTasks;
-                stringLength += toPrint.length();
                 System.out.println(toPrint);
             }
             if(allDone){
                 break;
             }
-            stringLength += poolList.size();
             allDone = true;
             Thread.sleep(200);
-            stringLength = 0;
             CLEARSCREEN();
         }
 
