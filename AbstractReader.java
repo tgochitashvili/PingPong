8243355bd249bf27dc8a90abedae434d294d15ce
tgochitashvili <tazo.gochitashvili@gmail.com>
2022-10-22 11:58:47 +0400

    Changed definitions

--- AbstractReader.java
+++ AbstractReader.java
@@ -1,14 +1,14 @@
-import java.util.List;
+import java.util.LinkedList; 
 
 public interface AbstractReader {
     /**
      * This method will look for urls in the urlPath, look for the token in every url, and replace them
      * with servers from serverPath and return ProcessNode lists for each server.
      */
-    public List<ProcessNode> getProcessNodesFromSource(String urlPath, String serverPath, String token);
+    public LinkedList<ProcessNode> getProcessNodesFromSource(String urlPath, String serverPath, String token);
     /*
      * This method will look for urls in the urlPath and return a single ProcessNode list.
      */
-    public List<ProcessNode> getProcessNodesFromSource(String urlPath);
+    public LinkedList<ProcessNode> getProcessNodesFromSource(String urlPath);
 
 }
